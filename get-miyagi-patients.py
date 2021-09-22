434c13f9471de8fad912fd3703a9c59dcaf8eb2c
Takeshi Nagae <nagae@tohoku.ac.jp>
2021-09-22 17:16:36 +0900

    年代別データを自動的に更新するスクリプト

--- get-miyagi-patients.py
+++ get-miyagi-patients.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 import datetime as dt
+from itertools import product
 url = "https://www.pref.miyagi.jp/pdf/covid19/m-covid-kanja.xlsx"
 all_patients_fname = "CoVid19-Miyagi-all_patients.csv"
 daily_patients_fname = "CoVid19-Miyagi-daily_patients_by_age.csv"
@@ -29,19 +30,18 @@ df.to_csv("data/{}".format(all_patients_fname), encoding='utf-8')
 df_url = 'https://raw.githubusercontent.com/nagae/CoVid-19/main/data/{}'.format(daily_patients_fname)
 df = pd.read_csv(df_url, header=[0,1], index_col=0)
 df = df.set_index(pd.to_datetime(df.index))
-
 # 最近の患者データ
 ages = ["10歳未満", "10代", "20代", "30代", "40代", "50代", "60代", "70代", "80代", "90歳以上"]
 states = ["入院中", "入院調整中", "療養中", "合計"]
 mult_cols = pd.MultiIndex.from_tuples(product(ages, states))
 all_patients_df_url = 'https://raw.githubusercontent.com/nagae/CoVid-19/main/data/{}'.format(all_patients_fname)
 all_patients_df = pd.read_csv(all_patients_df_url, index_col = 0, header=0)
-latest_dt = pd.to_datetime(patient_df.index.name) # 最近の患者データの更新日
-latest_dt = dt.datetime(latest_dt.year, last_dt.month, last_dt.day)
+latest_dt = pd.to_datetime(all_patients_df.index.name) # 最近の患者データの更新日
+latest_dt = dt.datetime(latest_dt.year, latest_dt.month, latest_dt.day)
 
 # 最近のデータがもともとのデータに反映されていない場合
 if latest_dt not in df.index:
-    new_row_df = pd.DataFrame(patient_df[ patient_df["患者_療養状況"] != "退院等"].groupby(["患者_年代", "患者_療養状況"]).count()["公表_年月日"]).T
+    new_row_df = pd.DataFrame(all_patients_df[ all_patients_df["患者_療養状況"] != "退院等"].groupby(["患者_年代", "患者_療養状況"]).count()["公表_年月日"]).T
     rdf = pd.DataFrame(columns=mult_cols, index = [latest_dt])
     for c in mult_cols:
         if c in new_row_df.columns:
